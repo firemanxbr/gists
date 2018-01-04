0bb75d6e8426827a19926bc8e40b3fd46fa6ceef
Marcelo Barbosa <firemanxbr@firemanxbr.usersys.redhat.com>
2018-01-04 23:33:08 +0100

    added feature list_users_gists

--- gists.py
+++ gists.py
@@ -1,21 +1,57 @@
-"""Gists CLI
-"""
+'''Gists CLI
+'''
 from argparse import ArgumentParser
+from tabulate import tabulate
 
 import json
 import requests
 
 
+'''Config
+'''
+API = 'https://api.github.com/'
+GIT_IO = 'https://git.io/'
+
+# NEED CHANGE
 req = requests.get('https://api.github.com/gists')
 
 
 def list_users_gists(user=None):
-    req = requests.get('https://api.github.com/users/{0}/gists'.format(user))
+    req = requests.get('{0}users/{1}/gists'.format(API, user))
 
     if req.status_code == 200:
-        return req.json()
+        headers = ['File', 'Size', 'URL', 'Description']
+
+        rows = []
+
+        for output in req.json():
+            filename = list(output['files'].keys())[0]
+            size = output['files'][filename]['size']
+            url = output['html_url']
+
+            gitio = requests.post('{0}create'.format(GIT_IO),
+                                  {'url': '{0}'.format(url)}).text
+
+            short_url = '{0}{1}'.format(GIT_IO, gitio)
+
+            description = output['description']
+
+            rows.append(['{0}'.format(filename),
+                        '{0}'.format(size),
+                        '{0}'.format(short_url),
+                        '{0}{1}'.format(description[:30],'...')])
+
+        if len(rows) == 0:
+            return "Don't have gists for '{0}'".format(user)
+
+        else:
+            return tabulate(rows, headers, tablefmt='grid',
+                            showindex='always',
+                            numalign='center',
+                            stralign='center')
+
     else:
-        return None
+        return "Don't found the user: '{0}''".format(user)
 
 
 def main():
@@ -33,16 +69,7 @@ def main():
         print(json.dumps(req.json(), sort_keys=True, indent=4))
 
     if args.user:
-
-        query = list_users_gists(user=args.user[0])
-
-        for output in query:
-            filename = list(output['files'].keys())[0]
-            url = output['html_url']
-            description = output['description']
-            print('File: \t\t{0}'.format(filename))
-            print('Description: \t{0}'.format(description))
-            print('URL: \t\t{0}\n'.format(url))
+        print(list_users_gists(user=args.user[0]))
 
 
 if __name__ == '__main__':
